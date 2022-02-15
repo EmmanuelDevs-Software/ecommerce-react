dc8acfebfd4f081e6d5c13e5f19dff5a9f2bedd2
Emmanuel Davila <emmanuel.davila@altran.com>
2022-02-15 06:49:12 +0100

    starting with the sign-in page

---
+++ src/components/sign-in/sign-in.component.js
@@ -0,0 +1,46 @@
+import React from "react";
+import './sign-in.styles.scss';
+import FormInput from "../form-input/form-input.component";
+import CustomButton from "../custom-button/custom-button.component";
+
+class SignIn extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: '',
+            password: ''
+        }
+    }
+
+    handleSubmit = (evt) => {
+        evt.preventDefault()
+        this.setState({email: '', password: ''})
+    }
+
+    handleChange = (evt) => {
+        const {value, name} = evt.target;
+        //Dinamic value
+        this.setState({[name]: value})
+    }
+
+    render() {
+        return (
+            <div className="sign-in">
+                <h2>I already have an account</h2>
+                <span>Sign in with your email and password</span>
+
+                <form onSubmit={this.handleSubmit}>
+                    <FormInput name="email" type="email" value={this.state.email} handleChange={this.handleChange}
+                               label="Email" required/>
+                    <FormInput name="password" type="password" value={this.state.password} label="Password"
+                               handleChange={this.handleChange}
+                               required/>
+                    <CustomButton type="submit" value="submit form">Sign In</CustomButton>
+                </form>
+
+            </div>
+        )
+    }
+}
+
+export default SignIn
