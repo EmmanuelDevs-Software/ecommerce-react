a66e9a5dbced52512e76cc6e6e9bb4b2eaaede57
Emmanuel Davila <emmanuel.davila@altran.com>
2022-02-24 13:55:32 +0100

    adding the persis-shop data . using redux-persist

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,9 +1,22 @@
-import { combineReducers } from 'redux';
+import { combineReducers } from "redux";
 
-import userReducer from './user/user.reducer';
-import cartReducer from './cart/cart.reducer';
+import userReducer from "./user/user.reducer";
+import cartReducer from "./cart/cart.reducer";
+import { persistReducer } from "redux-persist";
+//LocalStorage
+import storage from "redux-persist/lib/storage";
+/**
+ * import sessionstorage from 'redux-persist/lib/storage';
+ */
 
-export default combineReducers({
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["cart"],
+};
+
+const rootReducer = combineReducers({
   user: userReducer,
-  cart: cartReducer
+  cart: cartReducer,
 });
+export default persistReducer(persistConfig, rootReducer);
