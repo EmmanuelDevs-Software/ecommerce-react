f7c0bb67e8ec549b61e8264010e7cfc013189787
Emmanuel Davila <emmanuel.davila@altran.com>
2022-03-07 06:24:18 +0100

    changes

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import StripeCheckout from "react-stripe-checkout";
-
+import axios from 'axios'
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
   const publishableKey =
     "pk_test_51KY0aBJdmT4zGBTeC9wDFYB2zBxotOmhd8rccIoIAfuQ1STpmk2jylUMvHI8NzR6ug5wY85z9eY2cWHSNqdH7xn700fkxxv39Y";
   const onToken = (token) => {
     console.log(token);
+    
     alert("Payment Succesful!");
+
   };
 
   return (
